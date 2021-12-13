20108c065c0fad28cdc0968d46950eaa4e5cab42
Thanh Khan <k.coder.282@gmail.com>
2021-12-13 17:58:37 +0700

    first commit

---
+++ .cmd
@@ -0,0 +1,5 @@
+start php K:/workstation/CongNgheWeb/be/artisan serve --host 192.168.1.10 --port 8000
+start npm start --prefix K:/workstation/CongNgheWeb/fe/
+start D:\ApplicationTool\wamp64\wampmanager.exe
+code K:/workstation/CongNgheWeb/fe/
+code K:/workstation/CongNgheWeb/be/
